17d9645b970e9a8edab2cdb0b6a2917885f5f997
Linniue <148786112+Linniue@users.noreply.github.com>
2023-10-30 13:26:47 +0100

    iyf

---
+++ HTMLPage1.php
@@ -0,0 +1,101 @@
+﻿<?php echo bonjours?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Le titre de ma page</title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.cdnfonts.com/css/quastico" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/tasa-explorer" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/infinite-realities" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/lt-binary-neue" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/reddit-sans" rel="stylesheet">
+               
+                
+                
+        
+    </head>
+    <body>
+        <header>
+            <h1> Accueil </h1>
+        </header>
+                
+
+           
+        
+        <nav>
+            <a href="/HTMLPage1.html">Accueil</a> 
+                <a href="/HTMLPage1.html">caté 2</a>
+                <a href="/learn-javascript.html">caté 3</a>
+                <a href="/learn-php.html">caté 4</a>
+                <a href="/learn-php.html">caté 5</a>
+                <a href="/learn-php.html">caté 6</a>
+                <a href="/learn-php.html">caté 7</a>
+                <a href="/learn-php.html">caté 8</a>
+            
+        </nav>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+            <p style="text-align: justify;"></align>blablabla je suis un article très intéréssant blablabla je suis un article très intéréssant blablabla je suis un article très intéréssant blablabla je suis un article très intéréssant  </p>
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+        <br>
+        <div class="encadrer-un-contenu">
+            <h3>Article 1</h3> 
+
+
+        </div>
+
+        
+        
+    </body>
+</html>
